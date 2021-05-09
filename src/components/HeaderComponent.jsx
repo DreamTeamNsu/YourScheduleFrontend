ba5cc3ba7bc7ee42e5663cf95b4944cff4a90f2f
Arti66x <arti66x@gmail.com>
2021-05-09 23:30:24 +0700

    a

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -3,118 +3,125 @@ import ScheduleService from '../services/ScheduleService';
 import ScheduleTab from './ScheduleTab';
 
 class HeaderComponent extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-          groupNumber: '',
-          b1: '',
-          b2: '',
-          b3: '',
-          groupList: [],
-          b1List: [],
-          b2List: [],
-          b3List: [],
-        }
-        this.changeGroup = this.changeGroup.bind(this);
-        this.changeSpec = this.changeSpec.bind(this);
+  constructor(props) {
+    super(props)
+    this.state = {
+      groupNumber: '',
+      b1: '',
+      b2: '',
+      b3: '',
+      groupList: [],
+      b1List: [],
+      b2List: [],
+      b3List: [],
     }
-    componentDidMount(){              //запускаяется автоматически
-      ScheduleService.getGroups().then((res) =>{
-          this.setState({groupList: res.data})
-      });
-    }
-    changeGroup= (event) =>{
-      this.setState({groupNumber: event.target.value}); 
-      ScheduleService.getGroupTimetableAndSpecCourses( event.target.value).then((res) =>{
-        // console.log(res.data); //<==== в res.data.timetable расписание
-        // console.log(res.data.specCourses);
-        if(res.data.specCourses['1']){
+    this.changeGroup = this.changeGroup.bind(this);
+    this.changeSpec = this.changeSpec.bind(this);
+  }
+  componentDidMount() {              //запускаяется автоматически
+    ScheduleService.getGroups().then((res) => {
+      this.setState({ groupList: res.data })
+    });
+  }
+  changeGroup = (event) => {
+    this.setState({ groupNumber: event.target.value });
+    ScheduleService.getGroupTimetableAndSpecCourses(event.target.value).then((res) => {
+      // console.log(res.data); //<==== в res.data.timetable расписание
+      // console.log(res.data.specCourses);
+      if (res.data.specCourses['1']) {
 
-          this.setState({b1List: res.data.specCourses['1'],     //не факт что работает как нужно
-            b2List: res.data.specCourses['2'],
-            b3List: res.data.specCourses['3']
-          })
+        this.setState({
+          b1List: res.data.specCourses['1'],     //не факт что работает как нужно
+          b2List: res.data.specCourses['2'],
+          b3List: res.data.specCourses['3']
+        })
       }
-        // res.data.timetable              // <=======
+      ScheduleTab.grouId = event.target.value
+      // res.data.timetable              // <=======
     });
-    }
-    changeSpec= (block, event) =>{        //Свойство "block" объявлено, но его значение не было прочитано. WTF&&
-                                          //не взлетит -> будет отдельно для каждого блока
-      this.setState({block: event.target.value}); 
-      ScheduleService.getSpecCourseTimetable(event.target.value).then((res)=>{
-        // res.data              // <=======
+  }
+  changeSpec = (block, event) => {        //Свойство "block" объявлено, но его значение не было прочитано. WTF&&
+    //не взлетит -> будет отдельно для каждого блока
+    this.setState({ block: event.target.value });
+    ScheduleService.getSpecCourseTimetable(event.target.value).then((res) => {
+      // res.data              // <=======
     });
-    }
+  }
 
-    render() {
-        return ( 
-<div>
-  <nav className="navbar navbar-dark bg-dark text-white" >
-    <div className = "navbar-brand">Расписание НГУ ФИТ</div>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-  </nav>
-  <div className="collapse" id="navbarToggleExternalContent">
-    <div className="bg-dark text-white p-3">
-    <div className = "header-form"> 
-      <div className="container">
-        <div className="row justify-content-end">
-          <div className="col">
-            <div>Номер группы:</div>
-              <select onChange={this.changeGroup} value={this.state.groupNumber}>
-                <option value=''>None</option>
-                {
-                  this.state.groupList.map(
-                    item => <option value={item.groupNumber} key = {item.groupNumber}>{item.groupNumber}</option>
-                  )
-                }
-              </select>
-            </div>
-          <div className="col">Спецкурсы:</div>
-          <div className="col"> 
-            <div className="row">
-                <span className="text-muted"> b1:</span>                 
-                <select onChange={(e) => this.changeSpec('b1',e)} value={this.state.b1.id}>
-                  <option  value=''>None</option>
-                  {
-                    this.state.b1List.map(
-                      item => <option value={item.id} key = {item.id} >{item.name}</option>
-                    )
-                  }
-                </select>
-              </div>
-              <div className="row">
-                <span className="text-muted"> b2:</span>                 
-                <select onChange={(e) => this.changeSpec('b2',e)} value={this.state.b2.id}>              
-                  <option  value=''>None</option>
-                  {
-                    this.state.b2List.map(
-                      item => <option value={item.id}  key = {item.id}>{item.name}</option>
-                    )
-                  }
-                </select>
-              </div>
-              <div className="row">
-                <span className="text-muted"> b3:</span>                 
-                <select onChange={(e) => this.changeSpec('b3',e)} value={this.state.b3.id}> 
-                <option  value=''>None</option>
-                  {
-                    this.state.b3List.map(
-                      item => <option  value={item.id} key = {item.id}>{item.name}</option>
-                    )
-                  }
-                </select>
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-dark bg-dark text-white" >
+          <div className="navbar-brand">Расписание НГУ ФИТ</div>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </nav>
+        <div className="collapse" id="navbarToggleExternalContent">
+          <div className="bg-dark text-white p-3">
+            <div className="header-form">
+              <div className="container">
+                <div className="row justify-content-end">
+                  <div className="col">
+                    <div>Номер группы:</div>
+                    <select onChange={this.changeGroup} value={this.state.groupNumber}>
+                      <option value=''>None</option>
+                      {
+                        this.state.groupList.map(
+                          item => <option value={item.groupNumber} key={item.groupNumber}>{item.groupNumber}</option>
+                        )
+                      }
+                    </select>
+                  </div>
+                  <div id="spec">
+                    <div className="col">Спецкурсы:</div>
+                    <div className="col">
+                      <div className="row">
+                        <span className="text-muted"> b1:</span>
+                        <select onChange={(e) => this.changeSpec('b1', e)} value={this.state.b1.id}>
+                          <option value=''>None</option>
+                          {
+                            this.state.b1List.map(
+                              item => <option value={item.id} key={item.id} >{item.name}</option>
+                            )
+                          }
+                        </select>
+                      </div>
+                      <div className="row">
+                        <span className="text-muted"> b2:</span>
+                        <select onChange={(e) => this.changeSpec('b2', e)} value={this.state.b2.id}>
+                          <option value=''>None</option>
+                          {
+                            this.state.b2List.map(
+                              item => <option value={item.id} key={item.id}>{item.name}</option>
+                            )
+                          }
+                        </select>
+                      </div>
+                      <div className="row">
+                        <span className="text-muted"> b3:</span>
+                        <select onChange={(e) => this.changeSpec('b3', e)} value={this.state.b3.id}>
+                          <option value=''>None</option>
+                          {
+                            this.state.b3List.map(
+                              item => <option value={item.id} key={item.id}>{item.name}</option>
+                            )
+                          }
+                        </select>
+                      </div>
+                    </div>
+                  </div>
                 </div>
+              </div>
             </div>
           </div>
         </div>
-        </div>
+        <ScheduleTab groupNumber={this.state.groupNumber}/>
       </div>
-    </div>
-  </div>                          
-        );
-    }
+
+
+    );
+  }
 }
 
 export default HeaderComponent;
